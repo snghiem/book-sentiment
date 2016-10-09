9880495a12d5c7453bf291d55ab41d37acfb43e8
Sony Nghiem <sontnghiem@gmail.com>
2016-10-09 15:00:14 -0400

    Done with PNP

--- janeausten.R
+++ janeausten.R
@@ -1,27 +1,24 @@
+library(readr)
 library(syuzhet)
-library(ggplot2)
-library(stringr)
 library(magrittr)
-library(rvest)
-library(NLP)
-library(tidyr)
 
 # all of the following books obtained from Project Gutenberg
 
 # Pride and Prejudice
 # remove the header and footer
-PNP = read_lines("http://www.gutenberg.org/cache/epub/1342/pg1342.txt", skip = 30, n_max = 13060)
-raw = get_text_as_string("http://www.gutenberg.org/cache/epub/1342/pg1342.txt")
-PandP = get_sentences(raw)
 
-PNP2 = get_sentences(PNP)
+raw <- "http://www.gutenberg.org/cache/epub/1342/pg1342.txt" %>%
+  read_lines(skip = 30, n_max = 13060)
 
-sentiments = get_sentiment(PNP2, method="nrc")
-get_percentage_values(list.flatten(sentiments))
-plot(get_percentage_values(sentiments), main = "Pride and Prejudice", xlab = "Narrative Time", ylab = "Emotion", type="l")
+PNP <- raw %>%
+  get_sentences
 
-# not complete yet
-# Here are some references:
-# http://juliasilge.com/blog/If-I-Loved-NLP-Less/
-# http://juliasilge.com/blog/You-Must-Allow-Me/
-# http://rpubs.com/lmullen/58030
+sentiments <- PNP %>%
+  get_sentiment(method="nrc")
+sentiments = a
+
+sentiments %>%
+  get_percentage_values %>%W
+  plot(main = "Pride and Prejudice", xlab = "Narrative Time", ylab = "Emotion", type="l")
+abline(h=0, col=3, lty=2)
+
